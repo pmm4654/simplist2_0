9585d8c7b1069823a8172813e0884616b3ee832b
Patrick MacMurchy <pmm4654@gmail.com>
2018-10-24 19:05:13 -0400

    Update schema and delete a migration

--- db/migrate/20181022225453_create_items_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateItemsTable < ActiveRecord::Migration[5.1]
-  def change
-    create_table :items do |t|
-      t.string :title
-      t.references :list, foreign_key: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,6 @@ ActiveRecord::Schema.define(version: 20181023235411) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.bigint "list_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_items_on_list_id"
   end
 
