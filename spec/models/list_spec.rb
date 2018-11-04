6e9b58ba0c0c159a0d97147795ba9332a57bb395
Patrick MacMurchy <pmm4654@gmail.com>
2018-11-04 10:13:52 -0700

    Update list spec to expect relationship to registered_site

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe List do
   it { should belong_to(:user) }
+  it { should belong_to(:registered_site) }
   it { should validate_presence_of(:title) }
   it { should validate_uniqueness_of(:title).scoped_to(:user_id) }
 end
